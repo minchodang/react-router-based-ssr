8253ad1ce8e8bc64203536f3e26840f809e76e53
minchodang <minsu910725@gmail.com>
2025-01-05 14:07:12 +0900

    feat: ssr, prerender data

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import { route, type RouteConfig } from '@react-router/dev/routes';
+import { index, layout, route, type RouteConfig } from '@react-router/dev/routes';
 
-export default [route('contacts/:contactId', 'routes/contact.tsx')] satisfies RouteConfig;
+export default [
+    layout('layouts/sidebar.tsx', [index('routes/home.tsx'), route('contacts/:contactId', 'routes/contact.tsx')]),
+    route('about', 'routes/about.tsx'),
+] satisfies RouteConfig;
